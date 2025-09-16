fcac3f32ac8427e688895e155b9d29ac928b8836
Arni Magnusson <thisisarni@gmail.com>
2025-09-17 00:34:29 +1100

    Run model_shortcut by default

---
+++ TAF/model_full.R
@@ -0,0 +1,24 @@
+## Run analysis, write model results
+
+## Before: 13.par, skj.frq, skj.tag, doitall.sh, mfcl.cfg (boot/data),
+##         mfclo64 (boot/software)
+## After:
+
+library(TAF)
+
+mkdir("model")
+
+# Software
+cp("boot/software/mfclo64", "model")
+
+# Input files
+cp("boot/data/13.par",     "model")
+cp("boot/data/skj.tag",    "model")
+cp("boot/data/skj.frq",    "model")
+cp("boot/data/doitall.sh", "model")
+cp("boot/data/mfcl.cfg",   "model")
+
+# Run model
+setwd("model")
+system("doitall.sh")
+setwd("..")
