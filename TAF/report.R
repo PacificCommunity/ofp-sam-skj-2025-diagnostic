1a6b6b401726d6b5c116c0d8b1c44037e490faa5
Arni Magnusson <thisisarni@gmail.com>
2025-09-17 16:11:05 +1100

    Format tables

--- TAF/report.R
+++ TAF/report.R
@@ -1,9 +1,23 @@
 ## Prepare plots and tables for report
 
-## Before:
-## After:
+## Before: biology.csv, summary.csv (output)
+## After:  biology.csv, summary.csv (report)
 
 library(TAF)
 
 mkdir("report")
 
+# Read tables
+biology <- read.taf("output/biology.csv")
+summary <- read.taf("output/summary.csv")
+
+# Format tables
+biology <- rnd(biology, 2:5, c(1, 1, 3, 3))
+summary <- div(summary, 2:6, 10^c(6,3,3,3,3))
+summary <- rnd(summary, 2:8, c(0, 0, 0, 0, 0, 2, 2))
+biology <- format(biology)  # retain trailing zeros
+summary <- format(summary)  # retain trailing zeros
+
+# Write tables
+write.taf(biology, dir="report")
+write.taf(summary, dir="report")
